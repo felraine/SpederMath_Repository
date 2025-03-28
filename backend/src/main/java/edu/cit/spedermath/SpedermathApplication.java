2661788dd85d5d588297197b588abf0ef656ac19
Felraine Kyle A. Cataos <felrainekyle.cataos@cit.edu>
2025-03-28 18:42:11 +0800

    initialized frontend and backend

---
+++ backend/src/main/java/edu/cit/spedermath/SpedermathApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.spedermath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpedermathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpedermathApplication.class, args);
+	}
+
+}
