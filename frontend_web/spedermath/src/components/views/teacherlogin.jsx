aa3faee614d334b232f6f56249acf5a1ef7db7ec
Kinka Sugimoto <kinka.sugimoto@cit.edu>
2025-04-03 00:47:26 +0800

    updated teacher login UI

--- frontend_web/spedermath/src/components/views/teacherlogin.jsx
+++ frontend_web/spedermath/src/components/views/teacherlogin.jsx
@@ -8,18 +8,59 @@ function Login() {
     navigate("/register");
   };
 
+
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-    <div className="text-center p-6 bg-white rounded-lg shadow-lg">
-      <h2 className="text-2xl font-semibold mb-4">Login Page</h2>
-      <button 
-        className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
-        onClick={goToRegister}
-      >
-        Go to Register
-      </button>
+    
+    <>
+    <div className="flex flex-col h-screen">
+    <div className="flex items-start justify-start p-5 text-2xl font-bold text-black">
+      SpederMath
     </div>
-  </div>
+
+    <div className="flex justify-center items-center h-screen bg-white">
+      <div className="w-3/4 h-4/5 bg-white rounded-lg flex">
+        
+        {/* Left Side - Image to be pasted */}
+        <div className="w-1/2 flex items-center justify-center bg-gray-200 rounded-lg border">
+        <img 
+          src="login-image.png" 
+          alt="Login Illustration" 
+          className="w-150 h-124 object-fill object-cover rounded-lg "
+        />
+      </div>
+
+        {/* Right Side - Login Form */}
+        <div className="w-2/3 p-12 flex flex-col justify-center mb-6 ml-10">
+          <h2 className="text-6xl font-bold mb-5 text-center mb-6 ml-12">Login</h2>
+          <p className="text-black-600 mb-6 text-center mb-6 ml-12">Welcome back!</p>
+
+          <input 
+            type="email" 
+            placeholder="Email" 
+            className="w-full p-3 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300 mb-6 ml-10"
+          />
+          <input 
+            type="password" 
+            placeholder="Password" 
+            className="w-full p-3 mb-6 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300 mb-6 ml-10"
+          />
+
+          <button 
+            className="w-full px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg 
+            hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 mb-6 ml-10"
+            onClick={goToRegister}
+          >
+            Login
+          </button>
+          <p className="p-2 text-gray-600-sm text-center mt-4 mb-6 ml-10">
+          Don't have an account?{" "}
+          <a className="font-bold text-gray cursor-pointer hover:text-blue-600 " onClick={goToRegister}> Sign Up</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    </div>
+  </>
   );
 }
 
